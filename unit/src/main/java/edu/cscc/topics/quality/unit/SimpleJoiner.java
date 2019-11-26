48a227f7abd9617b253a1a647c9063667aef726f
CORP\flowera4 <ashtonflowers@gmail.com>
2019-11-25 22:46:08 -0500

    unit main fixed

--- unit/src/main/java/edu/cscc/topics/quality/unit/SimpleJoiner.java
+++ unit/src/main/java/edu/cscc/topics/quality/unit/SimpleJoiner.java
@@ -1,11 +1,17 @@
 package edu.cscc.topics.quality.unit;
 
-public class SimpleJoiner implements Joiner{
+public class SimpleJoiner implements Joiner {
 
     public String join(String separator, String base, String... parts) {
         String accumulation = base;
-        for (int i = 0; i < parts.length; i++) {
-            accumulation = accumulation + separator + parts[i];
+        if (parts != null) {
+            for (int i = 0; i < parts.length; i++) {
+                if (parts[i] != null) {
+                    accumulation = accumulation + separator + parts[i];
+                }
+            }
+        } else {
+            accumulation = base;
         }
         return accumulation;
     }
